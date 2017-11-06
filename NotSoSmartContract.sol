5db87def677e906f91173719a731745181601d91
AmruthPillai <im.amruth@gmail.com>
2017-11-06 10:07:45 +0530

    added functions

--- NotSoSmartContract.sol
+++ NotSoSmartContract.sol
@@ -6,15 +6,24 @@ contract NotSoSmartContract {
 
     // Other Data Types available on Solidity:
     // bool, int, uint, address (20 byte Ethereum Address), bytes, mapping (hash table), struct
-    uint public age;
-
-    // You can also define constants, so they cannot be changed later on
-    string constant firstName = 'Amruth';
+    string firstName;
+    uint age;
 
     // Every contract has a constructor they can use
     // They must have the same name as the contract
-    function NotSoSmartContract() public {
+    /* function NotSoSmartContract() public {
         age = 22;
+    } */
+
+    // Function to Set Details, to original variables
+    function setDetails(string _firstName, uint _age) public {
+        firstName = _firstName;
+        age = _age;
+    }
+
+    // Function to Get Details, returns values
+    function getDetails() public constant returns (string, uint) {
+        return (firstName, age);
     }
 
 }
